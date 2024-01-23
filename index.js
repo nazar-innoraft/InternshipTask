62e25bfbef5318257438c51d3a503147bfa43af7
nazar-innoraft <nazar.ali@innoraft.com>
2024-01-23 18:46:10 +0530

    navbar close effect modified

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.body.style.overflow = "hidden";
     document.body.style.paddingLeft = "250px";
-    
 }
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.body.style.overflow = "auto";
     document.body.style.paddingLeft = "0px";
 }
+document.querySelector(".maincontainer").addEventListener("mousedown", () => {
+    if (document.getElementById("mySidenav").style.width == "250px") {
+        document.getElementById("mySidenav").style.width = "0";
+        document.body.style.overflow = "auto";
+        document.body.style.paddingLeft = "0px";
+    }
+});
