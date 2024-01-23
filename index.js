c51e3921f03267b7a45e43bae6566abc86d7de63
nazar-innoraft <nazar.ali@innoraft.com>
2024-01-23 15:18:50 +0530

    navbar mdofied

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    document.body.style.overflow = "hidden";
+    document.body.style.paddingLeft = "250px";
+    
 }
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.body.style.overflow = "auto";
+    document.body.style.paddingLeft = "0px";
 }
